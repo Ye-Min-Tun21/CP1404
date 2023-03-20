8e8e217e35cb12ff4e735c9b30032c02bae89725
Ye Min Tun <yemintun@my.jcu.edu.au>
2023-03-20 15:20:07 +0800

    Add Practical File

--- Prac 2/password_stars.py
+++ Prac 2/password_stars.py
@@ -1,9 +1,18 @@
-minimum_password_length = 8
+def main():
+    minimum_password_length = 8
+    password = get_password(minimum_password_length)
+    print_asterisks(password)
 
-while True:
-    password = input("Enter a password: ")
-    if len(password) < minimum_password_length:
-        print("Password is too short. Please try again.")
-    else:
-        print("*" * len(password))
-        break
+def get_password(minimum_password_length):
+    while True:
+        password = input("Enter a password: ")
+        if len(password) < minimum_password_length:
+            print("Password is too short. Please try again.")
+        else:
+            return password
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+if __name__ == '__main__':
+    main()
