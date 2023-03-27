bdcdee5053f111b079f9ea7685a7853abfa87f56
Ye Min Tun <yemintun@my.jcu.edu.au>
2023-03-27 14:38:56 +0800

    Random Numbers

--- Prac 3/randoms.py
+++ Prac 3/randoms.py
@@ -4,6 +4,7 @@ The smallest number I could have seen is 5 and the largest number I could have s
 
 Line 2 - 9
 The smallest number I could have seen is 3 and the largest number I could have seen is 9.
+No, line 2 could not have produced a 4, because the step size is 2, which means only odd numbers between 3 and 10 (excluding 10) are possible outputs.
 
 Line 3 - 2.8201304330660664
 The smallest number I could have seen is 2.5 and the largest number I could have seen is 5.5.
