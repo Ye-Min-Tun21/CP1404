4cf069450743b6596689c6add45e9e66f7810ecd
Ye Min Tun <yemintun@my.jcu.edu.au>
2023-03-27 14:29:39 +0800

    Random Numbers

---
+++ Prac 3/randoms.py
@@ -0,0 +1,15 @@
+"""
+Line 1 - 16
+The smallest number I could have seen is 5 and the largest number I could have seen is 20.
+
+Line 2 - 9
+The smallest number I could have seen is 3 and the largest number I could have seen is 9.
+
+Line 3 - 2.8201304330660664
+The smallest number I could have seen is 2.5 and the largest number I could have seen is 5.5.
+"""
+
+import random
+
+num = random.randint(1, 101)
+print(num)
