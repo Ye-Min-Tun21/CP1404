390a59c670e49f9bf725917fb05b8a872bf5c482
Ye Min Tun <yemintun@my.jcu.edu.au>
2023-07-17 13:50:11 +0800

    Add prac 1 files

---
+++ prac_01/shop_calculator.py
@@ -0,0 +1,15 @@
+num_items = int(input("Number of items: "))
+while num_items < 0:
+    print("Invalid number of items!")
+    num_items = int(input("Number of items: "))
+
+total_price = 0
+for i in range(num_items):
+    item_price = float(input("Price of item: "))
+    total_price += item_price
+
+if total_price > 100:
+    total_price *= 0.9
+
+print(f"Total price for {num_items} items is ${total_price:.2f}")
+
